22cc3df15b233c11ecd8dffdcadf6c2c27711207
NoMoreNameToUse <zhangyichen380@gmail.com>
2020-07-21 23:03:14 +0200

    added log function

--- still_testing.js
+++ still_testing.js
@@ -23,6 +23,10 @@ const cooldown = new Discord.Collection();
 //dynamically retrieve all commands in the /command directory, filter only javascript files 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+//log the console log into a .txt file. 'a' means appending (old data will be preserved)
+const consoleLog = new console.Console(fs.createWriteStream('./log/logoutput.txt',{flags: 'a' }));
+consoleLog.log(`/*---------New Console Log startet at ${new Date()}---------*/`);
+
 /*########_Boot_#########*/
 /*-----------------------------Database-----------------------------*/
 //define sqlite database connection info 
@@ -45,6 +49,7 @@ const tempMemory = new Sequelize('sqlite::memory:');
 Still_testing.once('ready', () => {
     //display connect status in 
     console.log("Connected as " + Still_testing.user.tag)
+    consoleLog.log(`Successfuly connected as <@${Still_testing.user.tag}> at ${new Date()}` )
     const helloserver = Still_testing.guilds.cache.get(helloserverid)
     const hellochannel = helloserver.channels.cache.get(hellochannelid);
 
@@ -152,9 +157,11 @@ Still_testing.on('message', async message => {
     try {
         await command.execute(message, args, sqliteDB, tempMemory);
         console.log(`${message.author} used ${message} with ${args} as arguments`)
+        consoleLog.log(`#LOG <${new Date()}>: ${message.author} used ${message} with ${args} as arguments`);
     }
     catch (error) {
         message.reply(`Error executing command ${message},\n\n   Command:\n     ${message}\n   Error source:\n     ${error}.\n\nI definitly still need some testing`);
+        consoleLog.log(`#ERROR <${new Date()}>: ${error}`);
         console.error(error);
     }
 });

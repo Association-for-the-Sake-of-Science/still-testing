19d8f4147533bbcc86687d6990ca5f42ec64406e
NoMoreNameToUse <zhangyichen380@gmail.com>
2020-07-24 00:13:23 +0200

    reload

--- commands/shutup.js
+++ commands/shutup.js
@@ -12,76 +12,61 @@ module.exports = {
 	guildOnly: true,
 	category: 'Utility',
 	async execute(message, args) {
-		//get the command
+		//get the command and the target User 
 		const subCommand = args[0];
-		//list all User in shutup mode
-
-		// get the id of the target user
 		const ShutupUserId = message.mentions.users.first().id;
 		let ifUserid = false;
-
-		switch(subCommand){
-			case 'info':
-				if (shutuplist[0] == undefined) {
-					message.reply('no one is currently on shutup mode.');
+		//check if the target User is the bot itself. 
+		if (ShutupUserId == 732898098321293403 && subCommand === 'now') {
+			message.reply(`oops! i can't mute myself!`)
+			return
+		}
+		//set target user into shutup mode (put userid into shutuplist array) 
+		if (subCommand === 'now') {
+			for (i = 0; shutuplist[i] != undefined; i++) {
+				//check if the id already exists
+				if (shutuplist[i] == ShutupUserId) {
+					message.reply(`<@${ShutupUserId}> is already in shutup mode`)
+					ifUserid = true;
 				}
-				else {
-					const data = [];
-					data.push(`**there is currently ${shutuplist.length} User in shutup mode:**`);
-					for (i = 0; shutuplist[i] != undefined; i++) {
-						data.push(`     <@${shutuplist[i]}>`);
+			}
+			//push the id into array 
+			if (ifUserid == false) {
+				shutuplist.push(ShutupUserId)
+				activestate = true;
+				message.reply(`succesfully set <@${ShutupUserId}> into shutup mode`)
+			}
+			console.log(`Current User in shutup mode:${shutuplist}`)
+		}
+		//remove the user from shutuplist array 
+		else if (subCommand === 'withdraw' || subCommand === 'wd') {
+			let ifUserid = false;
+			for (i = 0; shutuplist[i] != undefined; i++) {
+				if (shutuplist[i] == ShutupUserId) {
+					shutuplist.splice(i, 1);
+					ifUserid = true;
+					if (shutuplist[0] == undefined) {
+						activestate = false;
 					}
-					data.push(`for more information please use the \`help\` command.`);
-					message.channel.send(data);
+					message.reply(`successfuly retracted the shutup on <@${ShutupUserId}>`)
 				}
-				return;
-				break;
-
-			case 'now':
-				//check if the user is bot himself
-				if(ShutupUserId == 732898098321293403){
-					message.reply(`oops! i can't mute myself!`);
-					return;
-				}
-
-				else{
-					//set target user into shutup mode (put userid into shutuplist array) 
-					for (i = 0; shutuplist[i] != undefined; i++) {
-						//check if the id already exists
-						if (shutuplist[i] == ShutupUserId) {
-							message.reply(`<@${ShutupUserId}> is already in shutup mode`);
-							ifUserid = true;
-						}
-					}
-					//push the id into array 
-					if (ifUserid == false) {
-						shutuplist.push(ShutupUserId);
-						activestate = true;
-						message.reply(`succesfully set <@${ShutupUserId}> into shutup mode`);
-					}
-					console.log(`Current User in shutup mode:${shutuplist}`);
-				}
-				break;
-
-			case 'withdraw' || 'wd':
-				//remove the user from shutuplist array 
-				ifUserid = false;
+			}
+			if (ifUserid) { message.reply(`<@${ShutupUserId}> is not in shutup mode`) }
+			console.log(shutuplist)
+		}
+		//list all User in shutup mode 
+		else if (subCommand === 'info') {
+			if (shutuplist[0] == undefined) {
+				message.reply('no one is currently on shutup mode.')
+			}
+			else {
+				const data = [];
+				data.push(`there is currently ${shutuplist.length} User in shutup mode:\n`)
 				for (i = 0; shutuplist[i] != undefined; i++) {
-					if (shutuplist[i] == ShutupUserId) {
-						shutuplist.splice(i, 1);
-						ifUserid = true;
-						if (shutuplist[0] == undefined) {
-							activestate = false;
-						}
-						message.reply(`successfuly retracted the shutup on <@${ShutupUserId}>`);
-					}
+					data.push(`<@${shutuplist[i]}>`)
 				}
-				if (ifUserid) { message.reply(`<@${ShutupUserId}> is not in shutup mode`) }
-				console.log(shutuplist);
-				break;
-
-			default:
-				return;
+				message.channel.send(data);
+			}
 		}
 	},
 	//check the message for shutup
@@ -93,7 +78,7 @@ module.exports = {
 				async () => {
 					//loop throgh the array to find the matching id 
 					const UserId = message.author.id;
-					console.log(shutuplist);
+					console.log(shutuplist)
 					for (i = 0; shutuplist[i] != undefined; i++) {
 						if (shutuplist[i] == UserId) {
 							return UserId;
@@ -111,5 +96,5 @@ module.exports = {
 					console.error(err);
 				})
 		}
-	}
+	},
 }
